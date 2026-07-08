e55275dc67d4f2f78f7c66bfe1aae5f6c0429e9e
is928joe-jpg <is928joe@gmail.com>
2026-07-08 18:42:20 +0800

    fix: 自动提交更新

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# 加载自定义包列表
 source /home/build/custom/ImmortalWrt/shell/custom-packages.sh
 
-# ImageBuilder 默认构建目录永远是 /home/build/openwrt
+# ImageBuilder 默认构建目录
 cd /home/build/openwrt || {
     echo "❌ ImageBuilder 构建目录不存在"
     exit 1
 }
 
-# 设备 Profile 映射
 case "$TARGET-$SUBTARGET" in
     "x86-64") DEVICE_PROFILE="generic" ;;
     "rockchip-armv8") DEVICE_PROFILE="friendlyarm_nanopi-r4s" ;;
@@ -19,20 +17,16 @@ esac
 
 echo "目标: $TARGET/$SUBTARGET | Profile: $DEVICE_PROFILE | 固件大小: ${ROM_SIZE}MB"
 
-# 构建固件
 make image \
   PROFILE="$DEVICE_PROFILE" \
   PACKAGES="$CUSTOM_PACKAGES" \
   FILES="/home/build/custom/ImmortalWrt/files" \
   ROOTFS_PARTSIZE=$ROM_SIZE
 
-# 检查构建结果
 if [ $? -eq 0 ]; then
     echo "✅ 构建成功"
 
     mkdir -p /home/build/custom/output
-
-    # 固件产物路径固定为 /home/build/openwrt/bin/targets
     cp -rv /home/build/openwrt/bin/targets/$TARGET/$SUBTARGET/* /home/build/custom/output/
 
     chmod -R 755 /home/build/custom/output
