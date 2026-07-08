33fdfc66e4c7a6b5664112292a1785c3c9010769
is928joe-jpg <is928joe@gmail.com>
2026-07-08 21:52:08 +0800

    fix: 自动提交更新

--- .github/workflows/update-options.sh
+++ .github/workflows/update-options.sh
@@ -5,7 +5,7 @@ shift 2
 
 OPTIONS=""
 for val in "$@"; do
-    OPTIONS+="          - $val"$'\n'
+    OPTIONS+="          - ${val}\n"
 done
 
 python3 << EOF
@@ -14,16 +14,17 @@ import re
 with open('.github/workflows/${WORKFLOW_FILE}', 'r') as f:
     content = f.read()
 
-# 匹配 options: [] 或 options: 后面的内容直到下一个非空字段
-new_content = re.sub(
+new_options = """${OPTIONS}"""
+
+content = re.sub(
     r'(${INPUT_NAME}:.*?options:\s*)\[.*?\]',
-    '\\1\n${OPTIONS}        ',
+    '\\1\\n' + new_options,
     content,
     flags=re.DOTALL
 )
 
 with open('.github/workflows/${WORKFLOW_FILE}', 'w') as f:
-    f.write(new_content)
+    f.write(content)
 
 print("Updated ${INPUT_NAME}")
 EOF
